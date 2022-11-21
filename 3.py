403795847510939f93a4120fd5ca9cbb7a3cfd3f
darabharath <118725395+darabharath@users.noreply.github.com>
2022-11-21 16:44:16 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,1 +1,18 @@
-
+valid=['0','1','2','5','6','8','9']
+n=int(input())
+l=[]
+u=1
+v=1
+while(v<=n):
+    str1=str(u)
+    x=0
+    for i in str1:
+        if(i in valid):
+            x+=1
+    if(x==len(str1)):
+        l.append(str1)
+        v+=1
+    u+=1
+f=l[len(l)-1]
+f=int(f)
+print(f)
